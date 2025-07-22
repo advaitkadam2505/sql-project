2286f22a16883eb9e92ca32d0a06c2860c0b816b
Advait Kadam <na22b023@smail.iitm.ac.in>
2025-07-23 01:57:27 +0200

    Create gold_fact_sales.sql

---
+++ scripts/gold_layer/gold_fact_sales.sql
@@ -0,0 +1,15 @@
+CREATE VIEW gold_fact_sales AS
+
+SELECT  
+sd.sls_ord_num as order_number, 
+dp.product_key,
+dc.customer_key,
+sd.sls_order_dt as order_date, 
+sd.sls_ship_dt as shipping_date, 
+sd.sls_due_dt as due_date, 
+sd.sls_sales as sales_amount, 
+sd.sls_quantity as quantity, 
+sd.sls_price as price
+FROM silver_crm_sales_details as sd
+LEFT JOIN gold_dim_products as dp ON sd.sls_prd_key= dp.product_number
+LEFT JOIN gold_dim_customers as dc ON sd.sls_cust_id= dc.customer_id;
